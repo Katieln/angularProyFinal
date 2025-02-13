88dc1c95e71f0be5af91b9d7205490e7cdf5f4b7
Katieln <139283838+Katieln@users.noreply.github.com>
2025-02-13 03:44:12 -0500

    t

---
+++ src/app/modules/dashboard/pages/courses/courses.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-courses',
+  standalone: false,
+  templateUrl: './courses.component.html',
+  styles: ``
+})
+export class CoursesComponent {
+
+}
---
+++ src/app/modules/dashboard/pages/enrollments/enrollments.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-enrollments',
+  standalone: false,
+  templateUrl: './enrollments.component.html',
+  styles: ``
+})
+export class EnrollmentsComponent {
+
+}

2dc59fe0f695ac224e3b8a76151ec0e6b729c919
Katieln <139283838+Katieln@users.noreply.github.com>
2025-02-13 03:47:42 -0500

    r1

--- src/app/modules/dashboard/dashboard-routing.module.ts
+++ src/app/modules/dashboard/dashboard-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CoursesModule } from './pages/courses/courses.module';
 
 
 //ruta base: /dasboard
@@ -15,6 +16,16 @@ const routes: Routes = [
     path: 'students',
     loadChildren: () =>
       import('./pages/students/students.module').then((m) => m.StudentsModule),
+  },
+  {
+    path: 'courses',
+    loadChildren: () =>
+      import('./pages/courses/courses.module').then((m) => m.CoursesModule),
+  },
+  {
+    path: 'enrollments',
+    loadChildren: () =>
+      import('./pages/enrollments/enrollments.module').then((m) => m.EnrollmentsModule),
   },
 ];
 
